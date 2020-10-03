5b011de40f331a3c157501e077f6e8d3ff4091b4
Gabriel Mascarenhas <gfmascarenhas@gmail.com>
2020-10-03 19:23:47 -0300

    Implement component Piu

---
+++ src/components/Piu/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState, useCallback, useEffect } from "react";
+import api from "../../services/api";
+import { infoPiu } from "../../pages/Feed/index";
+import LikeButton from "../../assets/images/like.svg";
+import FilledLikeButton from "../../assets/images/FilledLike.svg";
+import DeleteButton from "../../assets/images/x-mark.svg";
+
+import {
+    PiuContainer,
+    ProfilePictureContainer,
+    Actions,
+    DeleteButtonSpan
+} from "./styles";
+import { useAuth } from "../../hooks/useAuth";
+
+interface likeStateType {
+    likes: number;
+    touched: boolean;
+}
+
+interface PiuProps extends infoPiu {
+    isLiked: boolean;
+    searchPiuwers: string;
+    pius: Array<infoPiu>;
+    setPius(novosPius: Array<infoPiu>): void;
+}
+
+
+
+const Piu: React.FC<PiuProps> = ({ id ,usuario, likers, texto, searchPiuwers, pius, setPius, isLiked }) => {   
+
+    const { user } = useAuth();
+    const [piuVisibility, setPiuVisibility] = useState(true);
+
+    useEffect(() => {
+        const expressao = new RegExp(searchPiuwers, 'i');
+        setPiuVisibility(expressao.test(usuario.username));
+    }, [setPiuVisibility, searchPiuwers, usuario.username]);
+
+
+    const handleDelete = useCallback( async () => {
+        
+        setPiuVisibility(false);
+
+        const response = await api.delete(`/pius/${id}/`);
+        console.log(response);
+    }, [ id ] )
+
+    const handleLike = useCallback(async () => {
+        // criar [] <- mudando os pius(estado) atualizando ele do jeito que eu quero, dps setando os proprio piu como esse cara
+        
+
+        const piusAtualizados = pius.map((piu) => {
+
+            if ( !isLiked && piu.id === id) {
+                return {
+                    ...piu,
+                    likers: [
+                        ...piu.likers,
+                        user
+                    ]
+                }
+            } else if (isLiked && piu.id === id ) {
+                return {
+                    ...piu, 
+                    likers: piu.likers.filter(liker => liker.id !== user.id)
+                }
+            } else {
+                return piu;
+            }
+            
+        });
+
+        setPius( piusAtualizados );
+
+        await api.post('pius/dar-like/', {
+            usuario: usuario.id,
+            piu: id,
+        } );
+    }, [ usuario, id, pius, setPius, isLiked, user ]);
+
+    return (
+        <PiuContainer piuVisibility={ piuVisibility } >
+            <ProfilePictureContainer>
+                <img src={ usuario.foto } alt="foto de perfil" />
+                <div>
+                    <h3 className="username">{ usuario.username }</h3>
+                    <p>{ texto }</p>
+                </div>
+            </ProfilePictureContainer>
+            <Actions>
+                <span onClick={ handleLike } ><p>{ likers.length }</p>
+                {/* if liked src = x else scr = y */}
+                    <img src={ isLiked ? FilledLikeButton : LikeButton } alt="botão de like" />
+                </span>
+            </Actions>
+            
+            <DeleteButtonSpan onClick={ handleDelete } yourPiu={ usuario.id === user.id }>
+                <img src={DeleteButton} alt="botão para salvar o piu" />
+            </DeleteButtonSpan>
+        </PiuContainer>
+    )
+}
+export default Piu
---
+++ src/components/Piu/styles.ts
@@ -0,0 +1,104 @@
+import styled, { css } from "styled-components";
+
+interface DeleteButtonSpanProps {
+    yourPiu: boolean;
+} 
+
+interface PiuContainerProps {
+    piuVisibility: boolean;
+}
+
+export const PiuContainer = styled.li<PiuContainerProps>`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 2px solid #CED4DA;
+    padding: 20px;
+    transition: opacity .5s;
+    position: relative;
+    ${ props => !props.piuVisibility &&
+            css`
+                display: none;
+            `        
+    }
+`
+
+export const ProfilePictureContainer = styled.div`
+    display: flex;
+
+    img {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+        margin-right: 40px;
+        object-fit: cover;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        width: 450px;
+    }
+
+    h3 {
+        margin-bottom: 16px;
+        font-weight: 650;
+        font-size: 14px;
+    }
+`
+
+export const Actions = styled.div`
+    justify-self: flex-end;
+    align-self: flex-end;
+    margin-right: 5px;
+    display: flex;
+
+    span {
+        display: flex;
+        align-items: baseline;
+        justify-content: center;
+        color: #adb5bd94;
+        cursor: pointer;
+    }
+
+    img {
+        width: 26px;
+        height: 26px;
+        margin: 0 10px 0 7px;
+    }
+
+    img:hover {
+        opacity: 0.5;
+        transition: .3s;
+    }
+`
+export const DeleteButtonSpan = styled.span<DeleteButtonSpanProps>`
+    ${ props => props.yourPiu ?
+        css`
+            display: flex;
+            align-items: baseline;
+            justify-content: center;
+            color: #adb5bd94;
+            cursor: pointer;
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            img {
+                width: 16px;
+                height: 16px;
+                margin: 0 10px 0 7px;
+            }
+
+            img:hover {
+                opacity: 0.5;
+                transition: .3s;
+            }
+        `  :
+
+        css`
+            display: none;
+        `
+    }
+`
